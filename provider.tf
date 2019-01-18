b77cafce7a4d23deb5aec76199f8db3650606d23
Nemo <me@captnemo.in>
2019-01-19 03:39:29 +0530

    [doc] Adds note about empty provider block
    - See https://git.io/fh0qw

--- provider.tf
+++ provider.tf
@@ -6,6 +6,15 @@ provider "aws" {
   alias = "route53_account"
 }
 
-// Do not remove, this causes input prompts otherwise
-// known terraform bug
+/**
+ * Do not remove, this causes input prompts otherwise
+ * >At this time it is required to write an explicit 
+ * >proxy configuration block even for default (un-aliased)
+ * >provider configurations when they will be passed via
+ * >an explicit providers block
+ * 
+ * https://www.terraform.io/docs/modules/usage.html#passing-providers-explicitly
+ * https://git.io/fh0qw
+ */
+
 provider "aws" {}
