3636d4523a2e9c1fa7d488ae0862986739d0f07b
Hector Castro <hectcastro@gmail.com>
2019-04-26 12:25:56 -0400

    Add option for DNS validation record overwrite
    Changes to the `aws_route53_record` creation process made it so that
    DNS record overwrites now fail by default. In order to allow DNS record
    overwrites, the `allow_overwrite` attribute on the `aws_route53_record`
    resource must be enabled.
    
    The `allow_validation_record_overwrite` variable was added to support
    setting the value of `allow_overwrite` at the module level.

--- main.tf
+++ main.tf
@@ -10,11 +10,12 @@ resource "aws_route53_record" "validation" {
   provider = "aws.route53_account"
   count    = "${length(var.subject_alternative_names) + 1}"
 
-  name    = "${lookup(aws_acm_certificate.default.domain_validation_options[count.index], "resource_record_name")}"
-  type    = "${lookup(aws_acm_certificate.default.domain_validation_options[count.index], "resource_record_type")}"
-  zone_id = "${var.hosted_zone_id}"
-  records = ["${lookup(aws_acm_certificate.default.domain_validation_options[count.index], "resource_record_value")}"]
-  ttl     = "${var.validation_record_ttl}"
+  name            = "${lookup(aws_acm_certificate.default.domain_validation_options[count.index], "resource_record_name")}"
+  type            = "${lookup(aws_acm_certificate.default.domain_validation_options[count.index], "resource_record_type")}"
+  zone_id         = "${var.hosted_zone_id}"
+  records         = ["${lookup(aws_acm_certificate.default.domain_validation_options[count.index], "resource_record_value")}"]
+  ttl             = "${var.validation_record_ttl}"
+  allow_overwrite = "${var.allow_validation_record_overwrite}"
 }
 
 resource "aws_acm_certificate_validation" "default" {
